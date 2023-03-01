c40db9d9d56186d19da3e3a00dc03a4f8d254b07
tomaszgrazul <tgra@wp.pl>
2023-03-01 16:30:16 +0100

    zrobione

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.getElementsByClassName('mobile-hamburger')[0].addEventListener('click', function() {
+    document.getElementsByClassName('open-menu-holder')[0].classList.toggle('open');
+});
+
+// document.getElementsByClassName('mobile-close')[0].addEventListener('click', function() {
+    document.getElementsByClassName('fa-times')[0].addEventListener('click', function() {
+    document.getElementsByClassName('open-menu-holder')[0].classList.toggle('open');
+})
