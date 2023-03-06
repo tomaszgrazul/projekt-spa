4b45a3981d6900c21c714449f3e3caea91354a73
tomaszgrazul <tgra@wp.pl>
2023-03-06 14:22:44 +0100

    zrobione

--- js/main.js
+++ js/main.js
@@ -6,3 +6,62 @@ document.getElementsByClassName('mobile-hamburger')[0].addEventListener('click',
     document.getElementsByClassName('fa-times')[0].addEventListener('click', function() {
     document.getElementsByClassName('open-menu-holder')[0].classList.toggle('open');
 })
+
+
+const createAppointment = (appointment) => {
+    const appointmentMessage = document.querySelector('.appointment-message');
+    
+
+    fetch('https://akademia108.pl/api/ajax/post-appointment.php', {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        mode: 'cors',
+        method: 'POST',
+        body: JSON.stringify(appointment)
+    })
+    .then(ress => ress.json())
+    .then(resJSON => {
+        console.log(resJSON);
+        appointmentMessage.classList.remove('error');
+        appointmentMessage.classList.add('send'); 
+        appointmentMessage.innerText = `Dziękujemy ${resJSON.appointment.name}. Zostałeś zapisany!`
+    });
+}
+
+document.getElementById('appointment-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const appointmentMessage = document.querySelector('.appointment-message');
+    
+    let formFields = document.getElementsByClassName('form-field');
+    let allFields = false;
+    let appointment = {
+        name: document.getElementById('appointment-name').value, 
+        email: document.getElementById('appointment-email').value,
+        service: document.getElementById('appointment-service').value,
+        phone: document.getElementById('appointment-phone').value,
+        date: document.getElementById('appointment-date').value,
+        time: document.getElementById('appointment-time').value,
+        message: document.getElementById('appointment-message').value
+    }
+
+    for(let i = 0; i<formFields.length; i++) {
+        if(formFields[i].value === '') {
+            allFields = false;
+            formFields[i].classList.add('error-form');
+        } else {
+            allFields = true;
+            formFields[i].classList.remove('error-form');
+        }
+    }
+    if(allFields) {
+        createAppointment(appointment);
+    } 
+    else {
+        appointmentMessage.classList.add('error');
+        appointmentMessage.innerText = 'Wypełnij wymagane pola';
+    }
+
+  
+})
